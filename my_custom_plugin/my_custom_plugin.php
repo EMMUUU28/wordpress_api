2580170a695ad27ec3edec41c35a0752ef0bb643
EMMUUU28 <francisgudinho1234@gmail.com>
2023-08-17 19:29:37 +0530

    added automatic coupons code

--- my_custom_plugin/my_custom_plugin.php
+++ my_custom_plugin/my_custom_plugin.php
@@ -51,7 +51,7 @@ if($_SERVER['REQUEST_METHOD'] === 'GET'){
         add_action('wp_loaded','product_details'); 
 
     }
-}
+}       
 
 #To Display popup
 add_action('wp_enqueue_scripts','popup');
@@ -59,8 +59,34 @@ add_action('wp_enqueue_scripts','popup');
 #To get order details 
 add_action( 'woocommerce_thankyou', 'order_details' );  
 
+// ck_f34999e141ce2689921adceed7be2e65303b674f -  Consumer key 
+
+// cs_21d90a059989a9b97f3e9db6e161f5795f993d94 -  Consumer secret 
 
 
+
+add_action( 'init', 'misha_apply_coupon_by_url' );
+function misha_apply_coupon_by_url() {
+	echo "hello";
+    $code="mango23";
+    if( wc_get_coupon_id_by_code( $code ) ) {
+        echo 'Yep, the coupon exists.';
+        return;
+    } else {
+        echo 'Does not exist.';
+        $coupon = array(
+            'post_title' => $code, // dynamic coupon code
+            'post_content' => '',
+            'post_status' => 'publish',
+            'post_author' => 1,
+            'post_type' => 'shop_coupon'
+        );
+        $new_coupon_id = wp_insert_post( $coupon );
+        echo 'created coupon';
+    
+    }
+    
+}
 
 
 
